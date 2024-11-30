7370634e1b944dcba68da87853607312f0a2bf3f
nvnhuy2903 <nhathuy2903@gmail.com>
2024-11-30 21:25:34 +0700

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,204 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser= require('body-parser')
+// Tạo một router
+const AccountModel = require('./models/accounts.js');
+// Định nghĩa một route trong router
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+// tinh toan tat ca so the vang cua 1 cau thu
+// id là id của cau thu
+app.get('/yellow/:id',(req,res,next)=>{
+    AccountModel.getYellow(req.params.id)
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
 })
+
+
+// tinh toan tat ca so the do cua 1 cau thu 
+// id là id của cau thu
+app.get('/red/:id',(req,res,next)=>{
+    AccountModel.getRed(req.params.id)
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+})
+
+
+
+// tinh toan tat ca so ban thang cua 1 cau thu 
+// id là id của cau thu
+app.get('/goal/:id',(req,res,next)=>{
+    AccountModel.getGoal(req.params.id)
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+})
+
+
+
+// danh sach cac cau thu co so ban thang nhieu nhat trong 1 giai dau
+// id là id cua mua giai
+app.get('/topscorer/:id',(req,res,next)=>{
+    AccountModel.getTopScorers(req.params.id)
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+})
+
+
+// danh sach cac mua giai trong csdl(nhom minh lam 1)
+app.get('/tournament',(req,res,next)=>{
+    AccountModel.getAllTournament()
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+})
+
+
+// danh sach cac cau thu trong csdl
+app.get('/players',(req,res,next)=>{
+    AccountModel.getAllPlayers()
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+})
+
+
+// danh sach cac clb trong csdl
+app.get('/clubs',(req,res,next)=>{
+    AccountModel.getAllClubs()
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+})
+
+
+
+// danh sach cac trong tai trong csdl
+app.get('/refs',(req,res,next)=>{
+    AccountModel.getAllRefs()
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+})
+
+
+// danh sach cac hlv trong csdl
+app.get('/coaches',(req,res,next)=>{
+    AccountModel.getAllCoaches()
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+})
+
+
+// api tim kiem cau thu dua tren keyword nhap vao tu nguoi dung
+// co dang /searchPlayer?keyword=bru
+app.get('/searchPlayer', (req, res) => {
+    const keyword = req.query.keyword ? req.query.keyword.toLowerCase() : '';
+    
+    if (!keyword) {
+        return res.status(400).json({ message: 'Keyword is required' }); // Handle empty keyword
+    }
+    AccountModel.getRelevantPlayers(keyword)
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+});
+
+
+// tuong tu cai tren
+app.get('/searchClub', (req, res) => {
+    const keyword = req.query.keyword ? req.query.keyword.toLowerCase() : '';
+    
+    if (!keyword) {
+        return res.status(400).json({ message: 'Keyword is required' }); // Handle empty keyword
+    }
+    AccountModel.getRelevantClubs(keyword)
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+});
+
+//tuong tu cai tren
+app.get('/searchRef', (req, res) => {
+    const keyword = req.query.keyword ? req.query.keyword.toLowerCase() : '';
+    
+    if (!keyword) {
+        return res.status(400).json({ message: 'Keyword is required' }); // Handle empty keyword
+    }
+    AccountModel.getRelevantRefs(keyword)
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+});
+
+
+//tuong tu cai tren
+app.get('/searchCoach', (req, res) => {
+    const keyword = req.query.keyword ? req.query.keyword.toLowerCase() : '';
+    
+    if (!keyword) {
+        return res.status(400).json({ message: 'Keyword is required' }); 
+    }
+    AccountModel.getRelevantCoaches(keyword)
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+});
+
+
+//lay thong tin cau thu thong qua id
+app.get('/getPlayer/:id', (req, res) => {
+    AccountModel.getPlayers(req.params.id)
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+});
+
+
+//lay lich su chuyen nhuong cau thu thong qua id
+app.get('/getHistory/:id', (req, res) => {
+    AccountModel.getHistory(req.params.id)
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+});
+
+//lay thong tin tat ca tran dau thong qua id mua giai
+app.get('/getAllMatch/:id', (req, res) => {
+    AccountModel.getMatches(req.params.id)
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+});
+
+
+
+//lay bang xep hang thong qua id mua giai
+app.get('/getRank/:id', (req, res) => {
+    AccountModel.getRank(req.params.id)
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+});
+
+
+//lay cac hlv cua mot doi bong qua id doi bong do
+app.get('/getCoaches/:id', (req, res) => {
+    AccountModel.getCoaches(req.params.id)
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+});
+
+
+//lay 2 doi bong cua tran dau qua id tran do
+app.get('/getTeamsforMatch/:id', (req, res) => {
+    AccountModel.getTeamforMatch(req.params.id)
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+});
+
+
+// lay ds ra sân gom ca chinh thuc va khong chinh thuc cua 1 tran dau qua id tran dau do
+app.get('/getLineup/:id', (req, res) => {
+    AccountModel.getLineups(req.params.id)
+        .then(data => res.json(data)) // Send the data back as a response
+        .catch(err => res.status(500).json('that bai')); // Handle errors
+});
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Ứng dụng đang lắng nghe trên port ${port}`);
+});
+
---
+++ models/accounts.js
@@ -0,0 +1,325 @@
+const mysql = require('mysql2');
+
+// Cấu hình kết nối MySQL
+const con = mysql.createConnection({
+  host: 'nhathuy-mysql-server-cr10.mysql.database.azure.com',      // Địa chỉ máy chủ MySQL
+  user: 'nhathuy123',           // Tên người dùng MySQL (mặc định là 'root')
+  password: 'Toilaai123.',           // Mật khẩu người dùng MySQL (nếu có, nếu không để trống)
+  database: 'btl_csdl_official' // Tên cơ sở dữ liệu cần kết nối
+});
+
+// Kết nối đến MySQL
+con.connect(function(err) {
+    if (err) {
+        console.error('Lỗi kết nối: ' + err.stack);
+        return;
+    }
+    console.log('Đã kết nối với MySQL ');
+});
+
+async function getYellow(id) {
+    return new Promise((resolve, reject) => { 
+        const query='SELECT GetYellowCardCount(?) as a';
+        con.query(query,[id], function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result[0].a); 
+        });
+    });
+}
+
+async function getRed(id) {
+    return new Promise((resolve, reject) => { 
+        const query='SELECT GetRedCardCount(?) as a';
+        con.query(query,[id], function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result[0].a); 
+        });
+    });
+}
+
+async function getGoal(id) {
+    return new Promise((resolve, reject) => { 
+        const query = 'SELECT GetGoals(?) AS goal';  // Sử dụng tham số '?' trong query
+        con.query(query, [id], function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err);  
+            }
+            // In ra tổng lương trả về từ function
+            resolve(result[0].goal);  
+        });
+    });
+}
+
+async function getTopScorers(id) {
+    return new Promise((resolve, reject) => { 
+        const query='call GetTopScorers(?)';
+        con.query(query,[id], function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result[0]); 
+        });
+    });
+}
+
+async function getAllTournament() {
+    return new Promise((resolve, reject) => { 
+        const query='select * from tournament';
+        con.query(query, function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result); 
+        });
+    });
+}
+
+async function getAllPlayers() {
+    return new Promise((resolve, reject) => { 
+        const query='select * from players';
+        con.query(query, function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result); 
+        });
+    });
+}
+
+async function getAllClubs() {
+    return new Promise((resolve, reject) => { 
+        const query='select * from clubs';
+        con.query(query, function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result); 
+        });
+    });
+}
+
+async function getAllRefs() {
+    return new Promise((resolve, reject) => { 
+        const query='select * from referee';
+        con.query(query, function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result); 
+        });
+    });
+}
+
+async function getAllCoaches() {
+    return new Promise((resolve, reject) => { 
+        const query='select * from coaches';
+        con.query(query, function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result); 
+        });
+    });
+}
+
+async function getRelevantPlayers(name) {
+    return new Promise((resolve, reject) => { 
+        const query = 'SELECT * FROM players WHERE LOWER(firstname) like ? or lower(lastname) LIKE ?';
+        con.query(query,[`%${name}%`,`%${name}%`], function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result); 
+        });
+    });
+}
+
+async function getRelevantClubs(name) {
+    return new Promise((resolve, reject) => { 
+        const query = 'SELECT * FROM clubs WHERE LOWER(firstname) like ? or lower(lastname) LIKE ?';
+        con.query(query,[`%${name}%`,`%${name}%`], function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            resolve(result); 
+        });
+    });
+}
+
+async function getRelevantCoaches(name) {
+    return new Promise((resolve, reject) => { 
+        const query = 'SELECT * FROM coaches WHERE LOWER(name) like ?';
+        con.query(query,[`%${name}%`], function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            resolve(result);
+        });
+    });
+}
+
+
+async function getRelevantRefs(name) {
+    return new Promise((resolve, reject) => { 
+        const query = 'SELECT * FROM referee WHERE LOWER(name) like ?';
+        con.query(query,[`%${name}%`], function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result); 
+        });
+    });
+}
+
+async function getPlayers(id) {
+    return new Promise((resolve, reject) => { 
+        const query = 'call GetPlayersForClub(?)';
+        con.query(query,[id], function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result[0],result[1]); 
+        });
+    });
+}
+
+async function getHistory(id) {
+    return new Promise((resolve, reject) => { 
+        const query = 'call GetPlayerHistory(?)';
+        con.query(query,[id], function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result[0],result[1]); 
+        });
+    });
+}
+
+
+async function getMatches(id) {
+    return new Promise((resolve, reject) => { 
+        const query = 'select * from matches where tournamentid=?';
+        con.query(query,[id], function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result); 
+        });
+    });
+}
+
+async function getCoaches(id) {
+    return new Promise((resolve, reject) => { 
+        const query = 'call GetCoachesForClub(?)';
+        con.query(query,[id], function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result[0]); 
+        });
+    });
+}
+
+async function getTeamforMatch(id) {
+    return new Promise((resolve, reject) => { 
+        const query = 'call GetTeamsForMatch(?)';
+        con.query(query,[id], function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result[0]); 
+        });
+    });
+}
+
+
+async function getLineups(id) {
+    return new Promise((resolve, reject) => { 
+        const query = 'call GetStartingLineups(?)';
+        con.query(query,[id], function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result[0]); 
+        });
+    });
+}
+
+async function getRank(id) {
+    return new Promise((resolve, reject) => { 
+        const query = 'select * from ranking';
+        con.query(query,[id], function (err, result, fields) {
+            if (err) {
+                console.error('Lỗi khi gọi function: ' + err.stack);
+                return reject(err); 
+            }
+            
+            resolve(result); 
+        });
+    });
+}
+
+
+
+
+module.exports = {
+    getYellow,
+    getGoal,
+    getTopScorers,
+    getAllTournament,
+    getAllPlayers,
+    getAllClubs,
+    getAllRefs,
+    getAllCoaches,
+    getRelevantPlayers,
+    getRelevantClubs,
+    getRelevantCoaches,
+    getRelevantRefs,
+    getPlayers,
+    getHistory,
+    getMatches,
+    getCoaches,
+    getTeamforMatch,
+    getLineups,
+    getRank
+};
+
